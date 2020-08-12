ffe8d065ca0570989aa6c4d1496ef1cc1d22bff5
wayne <wayne@guweideMacBook-Pro.local>
2020-08-12 21:05:26 +0800

    [fixbug] 修复问题

--- app/router.ts
+++ app/router.ts
@@ -4,5 +4,6 @@ export default (app: Application) => {
   const { controller, router } = app;
   const { userAuth } = app.middleware;
   router.get('/scanv', controller.home.scanv);
+  router.get('/scanv', controller.oauth.basecallback);
   router.all('/api/*', userAuth(), controller.home.api);
 };

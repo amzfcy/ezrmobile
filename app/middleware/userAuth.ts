85d46a8a0423aa41805a78eea7d49452988fd3a0
wayne <wayne@guweideMacBook-Pro.local>
2020-08-12 23:15:32 +0800

    fixbug

--- app/middleware/userAuth.ts
+++ app/middleware/userAuth.ts
@@ -3,7 +3,7 @@ module.exports = () => {
     const { user } = ctx.session;
     const { brandId } = ctx.request.header;
     ctx.session.brandId = brandId || 429;
-    console.log(user);
+    console.log('user', user);
     if (user && user.SignStr) {
       await next();
     } else {

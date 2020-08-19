b2653da17af08b598377514c4fb8e42b64e667cf
wayne <wayne@guweideMacBook-Pro.local>
2020-08-19 22:46:36 +0800

    fixbug

--- config/plugin.ts
+++ config/plugin.ts
@@ -10,6 +10,10 @@ const plugin: EggPlugin = {
   //   enable: true,
   //   package: 'egg-view-assets',
   // }
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
 };
 
 export default plugin;
